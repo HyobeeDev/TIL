cb8fb4c4c2106628651881fabc6a9b68566da263
HyobeeDev <108445398+HyobeeDev@users.noreply.github.com>
2022-07-13 23:07:45 +0900

    team collabo backup
    KOKE

---
+++ KOKE/js/main.js
@@ -0,0 +1,14 @@
+$('.ham_btn').on('click', function(){
+  $('.ham_btn').toggleClass('active');
+  // $('nav').toggleClass('active');   // 나중에 넣기
+})
+
+//search - css 가능
+// const search = document.querySelector('#search')
+
+// search.addEventListener('focus', () => {
+//     search.setAttribute('placeholder', '검색어를 입력하세요')
+// })
+// search.addEventListener('blur', () => {
+//     search.setAttribute('placeholder', '')
+// })
