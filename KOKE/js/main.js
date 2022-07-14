7df7fd59c33dbf44d50e9336d309ee90b24b6d0b
HyobeeDev <108445398+HyobeeDev@users.noreply.github.com>
2022-07-14 23:38:23 +0900

    팀플 백업 220714
    메인페이지 백업

--- KOKE/js/main.js
+++ KOKE/js/main.js
@@ -12,3 +12,68 @@ $('.ham_btn').on('click', function(){
 // search.addEventListener('blur', () => {
 //     search.setAttribute('placeholder', '')
 // })
+
+
+
+
+
+// 2022 hot
+// let main = document.querySelector('.main_visual img')
+// let tab = document.querySelectorAll('.tab_imgs div img')
+
+// for(let i=0; i<tab.length; i++) {
+//     tab[i].onclick = changeSrc
+
+//     function changeSrc() {
+//         let selectTab = this.src
+//         main.setAttribute('src', selectTab)
+//     }
+// }
+
+
+// 2022 hot
+// new Swiper(".hot_swiper_thumbs", {
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+// new Swiper(".hot_swiper", {
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: swiper,
+//   },
+// });
+
+// 2022 hot - all__roasterys
+new Swiper('.all__roasterys .swiper', {
+  slidesPerView: '5',
+  spaceBetween: 50,
+  loop: true,
+  autoplay: {
+    delay: 1000
+  },
+  navigation: {
+    prevEl: '.all__roasterys .swiper-button-prev',
+    nextEl: '.all__roasterys .swiper-button-next'
+  }
+})
+
+// 2022 hot - review
+new Swiper('.review .swiper', {
+  slidesPerView: '4',
+  spaceBetween: 30,
+  // loop: true,
+  pagination: {
+    el: ".review .swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    prevEl: '.review .swiper-button-prev',
+    nextEl: '.review .swiper-button-next'
+  }
+})
