0e93c014a58828f92589985f21e54de03cfee3c1
HyobeeDev <108445398+HyobeeDev@users.noreply.github.com>
2022-07-19 17:45:48 +0900

    팀플 1차 완료 백업 220719
    1차 완료 및 제출

--- KOKE/js/main.js
+++ KOKE/js/main.js
@@ -1,53 +1,3 @@
-// top_bn
-let topBnClose = document.querySelector('.top_bn_close')
-let topBn = document.querySelector('.top_bn_container')
-
-topBnClose.addEventListener('click', () => {
-  topBn.style.height = '0'
-})
-
-
-$(function(){
-  // $(window).scroll(function(){
-  //     if($(this).scrollTop() >= 200){
-  //         $('nav').addClass('sticky')
-  //     } else {
-  //         $('nav').removeClass('sticky')
-  //     } // lnb event
-
-  //     if($(this).scrollTop() >= 200){
-  //         $('.top').fadeIn('fast')
-  //     } else {
-  //         $('.top').fadeOut('fast')
-  //     } // top event
-  // })
-
-  // $('.top_btn').click(function(e){
-  //     e.preventDefault()
-      
-  //     $('html, body').stop().animate({
-  //         scrollTop : 0
-  //     }, 500)
-  // }) // top btn
-
-  // $('.down_btn').click(function(e){
-  //     e.preventDefault()
-      
-  //     $('html, body').stop().animate({
-  //         scrollTop : 0
-  //     }, 500)
-  // }) // down btn
-
-  // $(window).scrollTo({top:0, left:0}, 500)
-
-  // $(window).scrollTo(this.hash || 0, 1000)
-
-})
-
-
-
-
-
 // 2022 hot
 // let main = document.querySelector('.main_visual img')
 // let tab = document.querySelectorAll('.tab_imgs div img')
@@ -62,23 +12,123 @@ $(function(){
 // }
 
 
-// 2022 hot
-// new Swiper(".hot_swiper_thumbs", {
-//   spaceBetween: 10,
-//   slidesPerView: 4,
-//   freeMode: true,
-//   watchSlidesProgress: true,
-// });
-// new Swiper(".hot_swiper", {
-//   spaceBetween: 10,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   thumbs: {
-//     swiper: swiper,
-//   },
-// });
+
+
+
+
+// 2022 main_visual
+new Swiper('.main_visual .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 3000
+  },
+  navigation: {
+    prevEl: '.main_visual .swiper-button-prev',
+    nextEl: '.main_visual .swiper-button-next'
+  }
+})
+
+
+
+// 모든 섹션들 나타나기
+const bestScrollUp = document.querySelector('.best')
+const newScrollUp = document.querySelector('.new')
+const hotScrollUp = document.querySelector('.hot')
+const reviewScrollUp = document.querySelector('.review')
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 500){
+    gsap.to(bestScrollUp, 0.7, {
+      opacity: 1,
+      y: -70
+    })
+  } else {
+    gsap.to(bestScrollUp, 0.7, {
+      opacity: 0,
+      y: 0
+    })
+  }
+}) // best
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 1350){
+    gsap.to(newScrollUp, 0.7, {
+      opacity: 1,
+      y: -70
+    })
+  } else {
+    gsap.to(newScrollUp, 0.7, {
+      opacity: 0,
+      y: 0
+    })
+  }
+}) // new
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 2800){
+    gsap.to(hotScrollUp, 0.7, {
+      opacity: 1,
+      y: -70
+    })
+  } else {
+    gsap.to(hotScrollUp, 0.7, {
+      opacity: 0,
+      y: 0
+    })
+  }
+}) // hot
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 4800){
+    gsap.to(reviewScrollUp, 0.7, {
+      opacity: 1,
+      y: -70
+    })
+  } else {
+    gsap.to(reviewScrollUp, 0.7, {
+      opacity: 0,
+      y: 0
+    })
+  }
+}) // review
+
+
+
+// best ==> 차후에 js로 바꾸기
+$(function () {
+
+	let $menuLabels = $('.teb__labels li a'); 
+	$menuLabels.on('click', function(e) {
+		e.preventDefault();
+		$menuLabels.removeClass('best_teb_active');
+		$(this).addClass('best_teb_active');
+
+		$('.item').fadeOut(100);
+		$(this.hash).delay(100).fadeIn();
+	});
+
+});
+
+
+// 2022 hot - 
+const thumbSwiper = document.querySelector(".hot_swiper_thumbs");
+
+var thumb_swiper = new Swiper(".hot_swiper_thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var main_swiper = new Swiper(".hot_swiper", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".hot .swiper-button-next",
+    prevEl: ".hot .swiper-button-prev",
+  },
+  thumbs: {
+    swiper: thumbSwiper.swiper
+  }
+});
 
 // 2022 hot - all__roasterys
 new Swiper('.all__roasterys .swiper', {
@@ -93,6 +143,8 @@ new Swiper('.all__roasterys .swiper', {
     nextEl: '.all__roasterys .swiper-button-next'
   }
 })
+
+
 
 // 2022 hot - review
 new Swiper('.review .swiper', {
