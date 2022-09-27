497269a30e4216892a5eeb04b2bdf1aef9ab33b5
HyobeeDev <hyobee.dev@gmail.com>
2022-09-27 19:42:14 +0900

    update: Day02

--- NomadCoders_Challenge_VanillaJs/momentum/app.js
+++ NomadCoders_Challenge_VanillaJs/momentum/app.js
@@ -1,1 +1,13 @@
-alert("hi")
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, "Hello ", false, null, true, undefined, "hyobee"];
+console.log(daysOfWeek, nonsense);
+
