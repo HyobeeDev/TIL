a9ba56b4676b441387b2c09a86e05fd76c58a1b1
HyobeeDev <hyobee.dev@gmail.com>
2022-09-27 20:17:39 +0900

    update: day02 done!

--- NomadCoders_Challenge_VanillaJs/momentum/app.js
+++ NomadCoders_Challenge_VanillaJs/momentum/app.js
@@ -1,13 +1,19 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const player = {
+  name: "Hyobee",
+  points: 10,
+  fat: true,
+};
 
-const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+console.log(player);
+console.log(player.name);
+console.log(player["fat"]); //이런 형식으로도 가능하다.
 
-const nonsense = [1, 2, "Hello ", false, null, true, undefined, "hyobee"];
-console.log(daysOfWeek, nonsense);
+player.fat = false; // 객체의 속성 값을 바꾸는 건 const도 가능하다.
+console.log(player.fat);
+// player = false; // 하지만 객체 자체를 바꾸는 건 불가능하다.
+// console.log(player.fat); 
+player.lastName = "Kim"; // 속성 추가도 가능하다.
+console.log(player.lastName); 
+player.points = player.points + 15; //  값을 더하는 것도 가능하다.
+console.log(player.points);
 
