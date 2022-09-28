c85249c256fb5e82a2b04bac9765fb04dc5630d8
HyobeeDev <hyobee.dev@gmail.com>
2022-09-28 10:16:03 +0900

    update: day03 start.

--- NomadCoders_Challenge_VanillaJs/momentum/app.js
+++ NomadCoders_Challenge_VanillaJs/momentum/app.js
@@ -1,19 +1,28 @@
-const player = {
-  name: "Hyobee",
-  points: 10,
-  fat: true,
-};
+/*
+// console.log("Hello! My name is nico");
+// console.log("Hello! My name is dal");
+// console.log("Hello! My name is lynn");
+// console.log("Hello! My name is hyobee");
 
-console.log(player);
-console.log(player.name);
-console.log(player["fat"]); //이런 형식으로도 가능하다.
+// 1. Hello! My name is가 자동으로 반복되게 만들기
+// function sayHello () {
+//   console.log("Hello! My name is 이름");
+// }
 
-player.fat = false; // 객체의 속성 값을 바꾸는 건 const도 가능하다.
-console.log(player.fat);
-// player = false; // 하지만 객체 자체를 바꾸는 건 불가능하다.
-// console.log(player.fat); 
-player.lastName = "Kim"; // 속성 추가도 가능하다.
-console.log(player.lastName); 
-player.points = player.points + 15; //  값을 더하는 것도 가능하다.
-console.log(player.points);
+// 3. 데이터 받기 (괄호 안에 넣어 준다)
+// function sayHello (nameOfPerson) {
+//   console.log(nameOfPerson);
+// }
+
+// 3. 두 개의 데이터 받기(콤마로 구분해준다)
+function sayHello (nameOfPerson, age) {
+  console.log("Hello, my name is " + nameOfPerson + " and I'm " + age + " years old.");
+}
+
+// 2. sayHello 실행버튼 만들고, 이름 데이터 보내기
+sayHello ("nico", 10);
+sayHello ("dal", 21);
+sayHello ("lynn", 13);
+sayHello ("hyobee", 22);
+*/
 
