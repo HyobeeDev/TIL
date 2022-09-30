d249035c55d828385b2806dd9cf5ed42cd19dcc1
HyobeeDev <hyobee.dev@gmail.com>
2022-09-30 21:49:53 +0900

    feat: Day05 start.

--- NomadCoders_Challenge_VanillaJs/momentum/app.js
+++ NomadCoders_Challenge_VanillaJs/momentum/app.js
@@ -164,10 +164,12 @@ if(isNaN(age) || age < 0) {
 // }
 */
 
+
+/*
 // 1.
 const h1 = document.querySelector(".hello h1");
 // const h1 = document.querySelectorAll(".hello h1");
-// console.dir(h1);
+console.dir(h1);
 // h1.innerText = "Hello!!!";
 // h1.style.color = "red";
 
@@ -205,14 +207,86 @@ h1.onmouseenter = handleMouseEnter;
 // h1.addEventListener("mouseleave", handleMouseLeave);
 h1.onmouseleave = handleMouseLeave;
 
-/**
- * 1. 이벤트 대상인 title을 선언한다.
- * 2. title에게 감지해야 할 이벤트를 정해주기 위해, 괄호 안에 어떤 상태일 때 어떤 함수를 실행할지를 정한다.
- * 3. 실행을 반복할 함수 코드를 만들어 준다.
- * => 내가 직접 실행버튼을 누르지 않고, JavaScript에 이 function이름을 넘겨줘서 유저가 title을 click할 경우에 JavaScript가 실행버튼을 대신 눌러주게끔 하는 것이다.
- */
+//  1. 이벤트 대상인 title을 선언한다.
+//  2. title에게 감지해야 할 이벤트를 정해주기 위해, 괄호 안에 어떤 상태일 때 어떤 함수를 실행할지를 정한다.
+//  3. 실행을 반복할 함수 코드를 만들어 준다.
+//  => 내가 직접 실행버튼을 누르지 않고, JavaScript에 이 function이름을 넘겨줘서 유저가 title을 click할 경우에 JavaScript가 실행버튼을 대신 눌러주게끔 하는 것이다.
 
 window.addEventListener("resize", handleWindowResize);
 window.addEventListener("copy", handleWindowCopy);
 window.addEventListener("offline", handleWindowOffline);
 window.addEventListener("online", handleWindowOnline);
+*/
+
+
+/* 과제1
+// <⚠️ DONT DELETE THIS ⚠️>
+const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+
+// - The text of the title should change when the mouse is on top of it.
+// - The text of the title should change when the mouse is leaves it.
+// - When the window is resized the title should change.
+// - On right click the title should also change.
+// - The colors of the title should come from a color from the colors array.
+// - DO NOT CHANGE .css, or .html files.
+// - ALL function handlers should be INSIDE of "superEventHandler"
+
+
+const title = document.querySelector(".hello h1");
+
+const superEventHandler = {
+  handleMouseEnter: function () {
+    title.innerText = "The mouse is here!👈🏻";
+    title.style.color = colors[0];
+  },
+  handleMouseLeave: function () {
+    title.innerText = "The mouse is gone...👉🏻";
+    title.style.color = colors[1];
+  },
+  handleWindowResize: function () {
+    title.innerText = "You just resized!🙀";
+    title.style.color = colors[2];
+  },
+  handleWindowContextmenu: function () {
+    title.innerText = "That was a right click!🖱";
+    title.style.color = colors[3];
+  }
+};
+
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener(
+  "contextmenu",
+  superEventHandler.handleWindowContextmenu
+);
+*/
+
+const h1 = document.querySelector(".hello h1");
+function handleTitleClick(){
+  // console.log(h1.style.color);
+  // h1.style.color = "blue"
+  // console.log(h1.style.color);
+  // 위의 세줄 코드를 통해 콘솔에서 현재 컬러값을 알아낼 수 있음을 확인.
+  
+  // 현재 컬러값이 OO이라면 XX로 바꿔주기 => if문
+  // 1. 직관적인 if문
+  // if(h1.style.color === "blue"){
+  //   h1.style.color = "tomato";
+  // } else {
+  //   h1.style.color = "blue"
+  // }
+
+  // 2. 읽기 좋은 형태로 바뀌기
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+h1.addEventListener("click", handleTitleClick);
