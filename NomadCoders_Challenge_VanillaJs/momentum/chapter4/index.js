b6a2c21075003c7d173159c3aa4173f6fe855dc7
HyobeeDev <hyobee.dev@gmail.com>
2022-10-03 22:56:14 +0900

    update: Day07 fin.

--- NomadCoders_Challenge_VanillaJs/momentum/chapter4/index.js
+++ NomadCoders_Challenge_VanillaJs/momentum/chapter4/index.js
@@ -1,3 +1,4 @@
+/*
 // element를 불러오는 방법 1
 // const loginForm = document.getElementById("login-form");
 // const loginInput = loginForm.querySelector("input");
@@ -23,8 +24,25 @@ function handleLoginBtnClick(){
   // }
   // 위의 유효성 체크는 HTML 안에서 Form의 input 속성을 통해서 진행 가능.
   console.log(username);
-
-  // 브라우저가 새로고침하지 않고 user 정보를 저장하도록 하려면?
 };
 
 loginButton.addEventListener("click", handleLoginBtnClick);
+*/
+
+
+// 브라우저가 새로고침하지 않고 user 정보를 저장하도록 하려면?
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event){
+  // console.username = loginInput.ariaValueMax;
+  // console.log("username");
+
+  // addEventListener의 첫 번째 인자는 이벤트이고 이벤트와 관련된 함수에서 그 이벤트에 대한 정보를 함수의 첫 번째 인자에 저장해둔다. 그 정보 안에는 preventDefault라는 메소드가 있는데 이것은 브라우저가 submit 했을 때 새로고침이 안 되게 막는 역할 등을 한다.
+  event.preventDefault(); // 브라우저의 기본 동작을 막아준다.
+  console.log(loginInput.value);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+// 변수명 뒤에 오는 ()괄호는 '당장 실행'하라는 명령어와 같다.
+// 그래서 addEventListener를 쓸 때, onLoginSubmit()에서 괄호를 쓰지 않는 이유는 당장 실행할 것이 아니기 때문이다.
