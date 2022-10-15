98ccd5e73c5c0acc230d6bdd504e5b9fe53677f1
HyobeeDev <hyobee.dev@gmail.com>
2022-10-15 13:43:27 +0900

    update: chapter7 Make localstorage forEach.

--- NomadCoders_Challenge_VanillaJs/momentum/chapter7/js/todo.js
+++ NomadCoders_Challenge_VanillaJs/momentum/chapter7/js/todo.js
@@ -3,6 +3,20 @@ const toDoInput = toDoForm.querySelector("input")
 // const toDoInput = document.querySelector("#todo-form input") // 윗줄과 동일한 코드임
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [
+
+]
+
+function saveToDos() {
+    // localStorage.setItem("todos",toDos);
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+    // JSON.stringify()는 입력되는 모든 것을 string으로 바꿔준다. 배열형태로 바뀜. JSON.parse()도 사용 가능
+
+}
+
+
 function deleteToDo(event) {
     // console.log(event);
     // console.dir(event.target);
@@ -37,8 +51,24 @@ function handleToDoSubmit(event) {
     // console.log(newTodo, toDoInput.value);
     toDoInput.value = ""; // 2. input에 내용을 치고 엔터를 누르면 새로고침은 막혔지만 값은 없어지게 하기
     // console.log(newTodo, toDoInput.value);
-
+    toDos.push(newTodo); // paint 하기 전에 toDos 배열에 밀어 넣어주기
     paintToDo(newTodo);
+    saveToDos();
 };
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// function sayHello(item) {
+//     console.log("This is turn of ", item);
+// };
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(saveToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    // console.log(parsedToDos); // 배열형태로 변경되었는지 확인하기
+    // parsedToDos.forEach(sayHello);
+    // forEach: 배열에 있는 각각의 item에 대해서 function을 실행해준다. 공평하게 배열안에 것들에게 다돌아가면서 한번씩 실행해주는 거다.
+    parsedToDos.forEach((item) => console.log("This is the turn of ", item));
+
+};
